5732405b58cf547d793e7476cdf74f642e495452
Aditya <91769994+Awesome-Tofu@users.noreply.github.com>
2024-03-13 22:46:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,15 @@ require('dotenv').config();
 
 module.exports = {
     "TOKEN": process.env.TOKEN || "your_token_here",
-    "CHANNEL_IDs": ["1015291642237943828", "737720047207972925", "988366298251657247", "761533912237080586"],
+    "CHANNEL_IDs": [
+        "1015291642237943828", 
+        "737720047207972925", 
+        "988366298251657247", 
+        "761533912237080586", 
+        "799949420808372274", 
+        "988366298251657247", 
+        "1055513658068844564", 
+        "842649958703693844"
+    ],
 }
 // Make sure to join the server of channel ID from the account you use the token from
